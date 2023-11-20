3c6695e4c94095e5ce0c9c839a27fdd0f47cc8a2
Aboo <112233500+ABDULRAHMANwoo@users.noreply.github.com>
2023-11-20 13:11:51 +0530

    5.py

--- CODEs/5.py
+++ CODEs/5.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the historical stock price data from a CSV file
-df = pd.read_csv(r"C:\Users\LENOVO\Downloads\WhatsApp Image 2023-11-02 at 09.39.30_85640aa8.jpg.csv")
+df = pd.read_csv(r"C:/Users/12202/Downloads/archive (1)/GOOGL.csv")
 # Create a DataFrame from the provided data (you can replace this with your data)
 
 # Convert the 'date' column to a datetime object
